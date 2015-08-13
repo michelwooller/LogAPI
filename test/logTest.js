2520c71452355f5afbe557a818424bc7279ff317
Michel Lucas Correa <michellucascorrea@Michels-MacBook-Pro.local>
2015-08-12 22:11:43 -0300

    RC 1

---
+++ test/logTest.js
@@ -0,0 +1,33 @@
+var logAPI = require("../index.js");
+var appenders_config = require("./appenders_config_example.json");
+
+
+logAPI.loadAppenderConfig(appenders_config);
+
+
+var logger1 = logAPI.createLogger("test.exec1");
+var logger2 = logAPI.createLogger("test.exec2");
+
+logger1.trace("[1] should not log");
+setTimeout(function(){
+	logger2.warn("[2] should log");
+	setTimeout(function(){
+		logger1.log("[1] should not log again");
+		setTimeout(function(){
+			logger2.info("[2] should log again");
+			setTimeout(function(){
+				logger1.info("[1] should log");
+				setTimeout(function(){
+					logger2.log("[2] should not log");
+					setTimeout(function(){
+						logger1.warn("[1] should log again");
+						setTimeout(function(){
+							logger2.trace("[2] should not log again");
+						}, 1000);
+					}, 1000);
+				}, 1000);
+			}, 1000);
+		}, 1000);
+	}, 1000);
+}, 1000);
+
