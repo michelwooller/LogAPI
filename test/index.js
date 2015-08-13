5916179d4c40ded96232ea46f3f72c41f2524def
Michel Lucas Correa <michellucascorrea@Michels-MacBook-Pro.local>
2015-08-12 22:23:11 -0300

    removing test/index.js

--- test/index.js
+++
@@ -1,7 +0,0 @@
-var logAPI = require("../index.js");
-var appenders_config = require("./appenders_config_example.json");
-
-console.log(logAPI);
-console.log(appenders_config);
-
-logAPI.loadAppenderConfig(appenders_config);
