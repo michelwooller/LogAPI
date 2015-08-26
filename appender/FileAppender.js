dcedcf06871603b59f5090509272f3ec87c1b0de
Michel Lucas Correa <michellucascorrea@Michels-MacBook-Pro.local>
2015-08-26 19:06:10 -0300

    first version of file appender

--- appender/FileAppender.js
+++ appender/FileAppender.js
@@ -1,47 +1,165 @@
-var file_system = require("fs");
+var FileSystem = require("fs");
+var Path = require('path');
 
 // config example
 /*
 {
-	
+	"file": {
+		"path": "folder/that/contains/the/file",
+		"name": "filename.extension"
+	},
+	"rotation" : {
+		"size" : 1, //byte or: kib mib gib, k, m, g (max file size)
+		"time" : 1, // second: or m, h, d (max file duration)		
+	}
 }
 */
 module.exports = function(name, print_pattern, config) {
 
 	// variables
-	var config = config;
-
 	this.name = name;
-	var print_pattern = print_pattern;
-
-	var file = undefined;
-
-	// methods
-	var fileUtil = {
-		"renew": function() {
-			if (file = undefined) {
-				fileUtil.create();
-			} else {
-				// check size
-				// if size is too much
-				//		create new file
-			}
-		},
-		"create" : function(){
-			
+	var _filename = undefined;
+	var _folder = undefined;
+	
+	/* initialization block */	
+	{
+		// initialize file location properties
+		if(config.file.path == undefined){
+			_folder = Path.normalize(Path.join(Path.resolve("./")));
+		} else {
+			_folder = Path.normalize(Path.join(Path.resolve(config.file.path)));
+		}
+		_filename = Path.normalize(Path.join(_folder, config.file.name));
+	}
+	
+	// internal methods
+	
+	/* create directory if needed */
+	function createLogDir(directory){
+		if (!FileSystem.existsSync(directory)) {
+			FileSystem.mkdirSync(directory, 0777);
 		}
 	}
 
-
+	// public methods
 	this.write = function(record) {
-		fileUtil.renew();
-
-		file.write(print_pattern.parse(record));
+		createLogDir(_folder);
+		
+		FileSystem.appendFile(_filename, print_pattern.parse(record) + "\n", function (e) {
+			if(e){
+				throw e;
+			}
+		});
 	};
 
 	this.release = function() {
-		// nothing to release
+		write_stream.end();
+		write_stream = undefined;
 	}
 
 	return this;
 };
+
+/*
+-- Testa acesso 
+access(path, FileSystem.R_OK | FileSystem.W_OK, callback) // accessSync
+-- testa existencia  
+exists(path, callback) // existsSync
+-- le arquivo
+readFile(path, {"encoding" : "utf-8"}, callback(err, data)) // readFileSync
+-- 
+
+  read: [Function],
+  readSync: [Function],
+  write: [Function],
+  writeSync: [Function],
+  rename: [Function],
+  renameSync: [Function],
+  truncate: [Function],
+  truncateSync: [Function],
+  ftruncate: [Function],
+  ftruncateSync: [Function],
+  rmdir: [Function],
+  rmdirSync: [Function],
+  fdatasync: [Function],
+  fdatasyncSync: [Function],
+  fsync: [Function],
+  fsyncSync: [Function],
+  mkdir: [Function],
+  mkdirSync: [Function],
+  readdir: [Function],
+  readdirSync: [Function],
+  fstat: [Function],
+  lstat: [Function],
+  stat: [Function],
+  fstatSync: [Function],
+  lstatSync: [Function],
+  statSync: [Function],
+  readlink: [Function],
+  readlinkSync: [Function],
+  symlink: [Function],
+  symlinkSync: [Function],
+  link: [Function],
+  linkSync: [Function],
+  unlink: [Function],
+  unlinkSync: [Function],
+  fchmod: [Function],
+  fchmodSync: [Function],
+  lchmod: [Function],
+  lchmodSync: [Function],
+  chmod: [Function],
+  chmodSync: [Function],
+  lchown: [Function],
+  lchownSync: [Function],
+  fchown: [Function],
+  fchownSync: [Function],
+  chown: [Function],
+  chownSync: [Function],
+  _toUnixTimestamp: [Function: toUnixTimestamp],
+  utimes: [Function],
+  utimesSync: [Function],
+  futimes: [Function],
+  futimesSync: [Function],
+  writeFile: [Function],
+  writeFileSync: [Function],
+  appendFile: [Function],
+  appendFileSync: [Function],
+  watch: [Function],
+  watchFile: [Function],
+  unwatchFile: [Function],
+  realpathSync: [Function: realpathSync],
+  realpath: [Function: realpath],
+  createReadStream: [Function],
+  ReadStream: 
+   { [Function: ReadStream]
+     super_: 
+      { [Function: Readable]
+        ReadableState: [Function: ReadableState],
+        super_: [Object],
+        _fromList: [Function: fromList] } },
+  FileReadStream: 
+   { [Function: ReadStream]
+     super_: 
+      { [Function: Readable]
+        ReadableState: [Function: ReadableState],
+        super_: [Object],
+        _fromList: [Function: fromList] } },
+  createWriteStream: [Function],
+  WriteStream: 
+   { [Function: WriteStream]
+     super_: { [Function: Writable] WritableState: [Function: WritableState], super_: [Object] } },
+  FileWriteStream: 
+   { [Function: WriteStream]
+     super_: { [Function: Writable] WritableState: [Function: WritableState], super_: [Object] } },
+  SyncWriteStream: 
+   { [Function: SyncWriteStream]
+     super_: 
+      { [Function: Stream]
+        super_: [Object],
+        Readable: [Object],
+        Writable: [Object],
+        Duplex: [Object],
+        Transform: [Object],
+        PassThrough: [Object],
+        Stream: [Circular] } } }
+*/
--- test/FileAppenderTest.js
+++ test/FileAppenderTest.js
@@ -1,89 +1,69 @@
-function l() {
-	console.log(arguments[0]);
-}
+var FileAppender = require("../appender/FileAppender.js");
+var PrintPattern = require("../PrintPattern.js");
+var Record = require("../Record.js");
+var Level = require("../Level.js");
+var FileSystem = require("fs");
+var Path = require('path');
 
+var print = console.log;
+var assert = console.assert;
 
-var file_system = require("fs");
+print("\n########################################################################");
+print(" LOG-API: logger tests");
+print("########################################################################\n\n");
 
-var file = undefined;
-var config = {
-	"file_name": "./out.log",
-	"rotation": {
-		"enabled": true, // true, false
-		"size": 30, // n (bytes), nK, nM, nKib, nMib
-		"date": "day", // hour, day, month
-		"startup": true //  true, false
-	}
-}
-
-var fileUtil = {
-	"renew": function() {
-		if (file == undefined) {
-			fileUtil.create();
-		} else {
-			// check size
-			// if size is too much
-			//		create new file
-		}
+var print_pattern = new PrintPattern("{out}"),
+file = null,
+config = {
+	"file": {
+		"path": "test/logs",
+		"name": "out.log"
 	},
-	"create": function() {
-		l(config.file_name);
-		file_system.exists(config.file_name, function(exists) {
-			if (exists) {
-				file_system.stat(config.file_name, function(err, stats) {
-					// bytes
-					l(stats.size);
-					l(stats.ctime);
-				});
-			}
-		});
+	"rotation" : {
+		"size" : 100, // kib mib gib, k, m, g (max file size - default bytes)
+		"time" : 60, // m, h, d (max file duration - default seconds)		
+	}
+};
+
+var file_abs_path = Path.normalize(Path.join(config.file.path, config.file.name));
+
+function removeFile(full_path){
+	if (FileSystem.existsSync(full_path)) {
+		FileSystem.unlinkSync(full_path);
 	}
 }
 
-fileUtil.renew();
+function writeLog(msg){
+	var record = new Record("test.logger", Level.trace, msg);
+	var file_appender = new FileAppender("FileAppenderName", print_pattern, config);
+	file_appender.write(record);	
+}
 
-/*
-     F_OK: 0,
-     R_OK: 4,
-     W_OK: 2,
-     X_OK: 1,
-     access: [Function],
-     exists: [Function],
-     readFile: [Function],
-     close: [Function],
-     open: [Function],
-     read: [Function],
-     write: [Function],
-     rename: [Function],
-     truncate: [Function],
-     ftruncate: [Function],
-     rmdir: [Function],
-     fdatasync: [Function],
-     fsync: [Function],
-     mkdir: [Function],
-     readdir: [Function],
-     fstat: [Function],
-     lstat: [Function],
-     stat: [Function],
-     readlink: [Function],
-     symlink: [Function],
-     link: [Function],
-     unlink: [Function],
-     fchmod: [Function],
-     lchmod: [Function],
-     chmod: [Function],
-     lchown: [Function],
-     fchown: [Function],
-     chown: [Function],
-     utimes: [Function],
-     futimes: [Function],
-     writeFile: [Function],
-     appendFile: [Function],
-     watch: [Function],
-     watchFile: [Function],
-     unwatchFile: [Function],
-     realpath: [Function: realpath],
-     // without Sync version
-	 createReadStream: [Function],
-     createWriteStream: [Function],
-*/
+/* validate the file content */
+removeFile(file_abs_path);
+writeLog("message");
+
+FileSystem.readFile(file_abs_path, "utf8", function(e, data) {
+	if (e) {
+		throw e;
+	} else {
+		assert("message" === data.trim(), "should be traced 'message'");
+	}
+});
+
+writeLog("message");
+
+FileSystem.readFile(file_abs_path, "utf8", function(e, data) {
+	if (e) {
+		throw e;
+	} else {
+		assert("message\nmessage" === data.trim(), "should be traced 'message'");
+	}
+});
+
+/* checks the file rotaion */
+
+
+
+print("FileAppender is ok");
+print("------------------------------------------------------------------------");
