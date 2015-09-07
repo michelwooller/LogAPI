867399c428518985fb78fe2c54e7aadfe9fef4f1
Michel Lucas Correa <michellucascorrea@Michels-MacBook-Pro.local>
2015-09-07 18:41:47 -0300

    teste do ConsoleAppender finalizados

--- appender/ConsoleAppender.js
+++ appender/ConsoleAppender.js
@@ -1,25 +1,30 @@
-// config example
-/*
-	none
-*/
 module.exports = function(name, print_pattern, config) {
 	// no custom config
+	var that = this;
 	var config = config;
-
-	this.name = name;
 	var print_pattern = print_pattern;
-
-	this.write = function(record) {
+	
+	this.name = name;
+	
+	this.writeSync = function(record) {
 		if (record.isError) {
 			process.stderr.write(print_pattern.parse(record) + "\n");
 		} else {
 			process.stdout.write(print_pattern.parse(record) + "\n");
 		}
 	};
-
-	this.release = function() {
-		// nothing to release
-	}
-
+	
+	this.write = function(record, callback) {
+		process.nextTick(function() {
+			try {
+				that.writeSync(record, directory);
+				callback(undefined);
+			} catch (e) {
+				callback(e);
+			}
+		});
+		
+	};
+	
 	return this;
 };
--- test/ConsoleAppenderTest.js
+++ test/ConsoleAppenderTest.js
@@ -1,66 +1,55 @@
-/*
-var ConsoleAppender = require("../appender/ConsoleAppender.js");
-var PrintPattern = require("../PrintPattern.js");
-var Level = require("../Level.js");
-var Record = require("../Record.js");
+'use strict';
+var ConsoleAppender = require('../appender/ConsoleAppender.js');
+var PrintPattern = require('../PrintPattern.js');
+var Record = require('../Record.js');
+var Level = require('../Level.js');
+var FileSystem = require('fs');
+var Path = require('path');
 
-var print = console.log;
-var assert = console.assert;
+var test = require('unit.js');
+var should = test.should;
 
-/*
-###########################################################
-###########################################################
-TODO review this test from head to tail. Something is wrong
-###########################################################
-###########################################################
-TODO review this test from head to tail. Something is wrong
-###########################################################
-###########################################################
-TODO review this test from head to tail. Something is wrong
-###########################################################
-###########################################################
-TODO review this test from head to tail. Something is wrong
-###########################################################
-###########################################################
-*/
+var record, print_pattern = new PrintPattern('{out}');
 
+describe('Suite de testes do ConsoleAppender.', function() {
+	describe('Testes construção.', function() {
+		describe('Dado uma instância do ConsoleAppender', function() {
+			var console_appender = new ConsoleAppender('SimpleConsoleAppender', print_pattern);
+			
+			describe('quando criada de forma simples', function() {
+				it('então deve conter a interface básica exigida para ser uma Appender.', function() {
+					should(console_appender.name).be.equal('SimpleConsoleAppender');
+					should(console_appender.write).be.type('function');
+					should(console_appender.writeSync).be.type('function');
+				});
+			});
+			
+			describe('quando escrito um record de level menor que error', function() {
+				it('então a mensagem deveria sair no stream de saída do sistema operacional.', function() {
+					var original_out = process.stdout.write;
+					process.stdout.write = function(chunk) {
+						process.stdout.write = original_out;
+						should(chunk).be.equal('mensagem_out\n');
+					}
 
-/*
-print("\n########################################################################");
-print(" LOG-API: console appender tests");
-print("########################################################################\n\n");
+					record = new Record('test.logger', Level.trace, 'mensagem_out');
+					console_appender.writeSync(record);
+				});
+			});
+			
+			describe('quando escrito um record de level igual a error', function() {
+				it('então a mensagem deveria sair no stream de erro do sistema operacional.', function() {
+					var original_err = process.stderr.write;
+					var error = new Error('mensagem_err')
+					process.stderr.write = function(chunk) {
+						process.stderr.write = original_err;
+						should(chunk).be.equal(error.stack + '\n');
+					}
 
-var console_appender = new ConsoleAppender("ConsoleAppender", new PrintPattern("{out}"));
-var e = new Error("message");
-
-var out_assertion_listener = function(data) {
-	print(">>>" + data);
-	assert("message" == data, "the output should be equals to 'message'");
-}
-
-var err_assertion_listener = function(data) {
-	print(data);
-	assert(e.stack == data, "the output should be equals to '" + e.stack + "'");
-}
-
-process.stdout.on("data", out_assertion_listener);
-process.stderr.on("data", err_assertion_listener);
-// -------------
-// -------------
-// -------------
-var record = new Record("test.logger", Level.trace, "message");
-console_appender.write(record);
-// -------------
-// -------------
-// -------------
-record = new Record("test.logger", Level.trace, e);
-console_appender.write(record);
-// -------------
-// -------------
-// -------------
-process.stdout.removeListener("data", out_assertion_listener);
-process.stderr.removeListener("data", err_assertion_listener);
-
-//print("console appender is ok");
-print("------------------------------------------------------------------------");
-*/
+					record = new Record('test.logger', Level.error, error);
+					console_appender.writeSync(record);
+				});
+			});
+		});
+	});
+});
